386428edaedebf16f92ad6ad09cedbaa66eb90de
Edward Magsino <edward.magsino@ghl.com>
2026-01-28 18:58:04 +0800

    Add error handling

--- NTTPacketParser/InputForm.cs
+++ NTTPacketParser/InputForm.cs
@@ -17,16 +17,23 @@ namespace NTTPacketParser
 
 		private void btnParse_Click(object sender, EventArgs e)
 		{
-			var parser = new PosMessageParser();
-			parser.Parse(txtInput.Text);
+			try
+			{
+				var parser = new PosMessageParser();
+				parser.Parse(txtInput.Text);
 
-			var resultForm = new ResultForm(
-				parser.Fields,
-				parser.OtherDetails,
-				this
-			);
+				var resultForm = new ResultForm(
+					parser.Fields,
+					parser.OtherDetails,
+					this
+				);
 
-			resultForm.Show();
+				resultForm.Show();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Error parsing packet: {ex.Message}", "Parse Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 	}
 }
